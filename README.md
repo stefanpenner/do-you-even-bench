3751fa3f18402d27533c979fa0f484dd59e8a7b0
Stefan Penner <stefan.penner@gmail.com>
2015-09-05 18:37:03 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Do you even bench
+
+```js
+require('do-you-even-bench', [
+  { name: 'test 1', fn: function() { ... } },
+  { name: 'test 2', fn: function() { ... } },
+  { name: 'test 3', fn: function() { ... } },
+]);
+
+```
---
+++ index.js
@@ -0,0 +1,35 @@
+var Benchmark = require('benchmark');
+
+function log(message) {
+  if (typeof window !== 'undefined') {
+    var div = document.createElement('div');
+    div.textContent = message;
+    document.getElementById('output').appendChild(div);
+  } else  {
+    console.log(message);
+  }
+}
+
+module.exports = function(suites) {
+  var suite = new Benchmark.Suite();
+
+  log('testing');
+  suites.forEach(function(s) {
+    log('- ' + s.name);
+    suite.add(s.name, s.fn);
+  });
+
+  suite.on('cycle', function(event) {
+    log(String(event.target));
+  })
+  .on('complete', function() {
+    log('Fastest is ' + this.filter('fastest').pluck('name'));
+  });
+
+  setTimeout(function() {
+    console.log('running first test, please wait...')
+    suite.run({
+      'async': true
+    });
+  }, 1000);
+}
